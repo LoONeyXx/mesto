a2c77978b46ce2c05eaf5b1c69e543057926594a
LoONeyXx <looneyxx@yandex.ru>
2023-02-16 14:12:28 +0300

    hotfix:createCards and fix other function

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,9 @@ const popupInputJob = popupEditProfile.querySelector(".popup__input_type_job");
 const popupBtnCloseTypeEdit = document.querySelector(".popup__btn-close");
 const popupImage = document.querySelector('.popup__image')
 const popupImageText = document.querySelector('.popup__image-text')
-const popupBtnCloseTypeAdd = document.querySelector(".popup__btn-close_type_add-cards");
-const popupForm = document.querySelectorAll(".popup__form");
+const popupFormEdit = document.querySelector(".popup__form_type_edit");
+const popupFormCards = document.querySelector(".popup__form_type_cards");
 const popupTypeImage = document.querySelector(".popup_type_image");
-
-
 /*   Перменные профиля           */
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
@@ -21,13 +19,14 @@ const profileAddBtn = document.querySelector(".profile__btn-add");
 
 //  Перменные карточек
 const cardsContainer = document.querySelector(".cards__container");
+const card = document.querySelector('#template-card').cloneNode(true).content
 const cardsTitleInput = document.querySelector(".popup__input_type_card-title");
 const cardsLinkInput = document.querySelector(".popup__input_type_card-link");
-const cardsImage = document.querySelectorAll(".cards__image");
-const templateCard = document.querySelector('.template-item')
+const closeBtns = document.querySelectorAll('.popup__btn-close')
+
 // Обьект названия и ссылки карточек
 
-let initialCards = [
+const initialCards = [
   {
     name: "Екатеринбург",
     link: "./images/ekat.png",
@@ -57,83 +56,80 @@ let initialCards = [
 /*       Функция добавления новых карточек         */
 
 function addedNewCard() {
-  const newCard = createCard()
-  const imgCard = newCard.querySelector(".cards__image");
-  const titleCard = newCard.querySelector(".cards__title");
-  const cardsTitleInput = document.querySelector(".popup__input_type_card-title");
-  const cardsLinkInput = document.querySelector(".popup__input_type_card-link");
-  titleCard.textContent = cardsTitleInput.value;
-  imgCard.src = cardsLinkInput.value;
+  const cardTitle = cardsTitleInput.value
+  const cardLink = cardsLinkInput.value
+  const newCard = createCard(cardTitle, cardLink)
   cardsContainer.prepend(newCard);
 }
+// Создание новой карточки
 {
-  function createCard() {
-    const newCard = document.querySelector('.template-item').content.cloneNode(true)
-    const imgCard = newCard.querySelector(".cards__image");
-    const imgCardTitle = newCard.querySelector(".cards__title");
-    const likeBtn = newCard.querySelector(".cards__like-btn");
-    const elementBtnDelete = newCard.querySelector(".cards__delete-btn");
-    const closeBtn = document.querySelector(".popup__btn-close_type_image");
-    closeBtn.addEventListener('click', () => closePopup(popupTypeImage))
-    elementBtnDelete.addEventListener("click", deletedCard);
+  function createCard(title, link) {
+    const newCard = card.cloneNode(true)
+    const titleCard = newCard.querySelector(".cards__title");
+    const imgCard = newCard.querySelector('.cards__image');
+    const imgCardTitle = newCard.querySelector('.cards__title');
+    const likeBtn = newCard.querySelector('.cards__like-btn');
+    const elementBtnDelete = newCard.querySelector('.cards__delete-btn');
+    elementBtnDelete.addEventListener('click', deletedCard);
     likeBtn.addEventListener("click", toogleLike);
+    titleCard.textContent = title
+    imgCard.src = link
+    imgCard.alt = title
     imgCard.addEventListener("click", () => {
       popupImage.src = imgCard.src
+      popupImage.alt = imgCard.alt
       popupImageText.textContent = imgCardTitle.textContent
       openPopup(popupTypeImage)
     })
+
     return newCard
   }
 }
 
+/*   Функция рендера Карточек        */
+
+function renderCards() {
+  const cardsArray = [];
+  for (let i = 0; i < initialCards.length; i++) {
+    const cardTitle = initialCards[i].name
+    const cardLink = initialCards[i].link
+    cardsArray[i] = createCard(cardTitle, cardLink)
+    cardsContainer.append(...cardsArray)
+  }
+
+  cardsContainer.append(...cardsArray)
+}
 
 /*   Функция удаления карточки  и добавление в массив InitialCards     */
+
 function deletedCard(evt) {
-  const delBtn = evt.target;
-  delBtn.parentElement.remove();
+  evt.target.closest('.cards__item').remove();
 }
 
-/*     Функция для рендера инпут полей     */
+/*     Функция для рендера инпут полей профиля    */
 
-function renderInput(popup) {
-  if (popup.classList.value.includes("popup_type_edit-profile")) {
-    popupInputName.value = profileTitle.textContent.trim();
-    popupInputJob.value = profileSubtitle.textContent.trim();
-  } else {
-    const cardsTitleInput = document.querySelector(".popup__input_type_card-title");
-    const cardsLinkInput = document.querySelector(".popup__input_type_card-link");
-    cardsTitleInput.value = "".trim();
-    cardsLinkInput.value = "".trim();
-  }
+function renderInputEdit() {
+  console.log(profileTitle.textContent)
+  popupInputName.value = profileTitle.textContent.trim();
+  popupInputJob.value = profileSubtitle.textContent.trim();
+
 }
+/*     Функция для рендера инпут полей добавления карточек    */
 
-/*     Функция переключение состояния кнопки Like, P.S Добавление транзишн через JS дает избавиться от бага который во время загрузки страницы делает попап видимым на некоторые время     */
+function renderInputAddCards() {
+  popupAddCards.querySelector('.popup__form').reset()
+  popupAddCards.querySelector('.popup__form').reset()
+}
+/*     Функция переключение состояния кнопки Like,*/
 
 function toogleLike(evt) {
-  const el = evt.target;
-  if (!el.classList.value.includes("cards__like-btn_active")) {
-    el.style["-webkit-transform"] = "scale(0) translateZ(0)";
-    el.classList.add("cards__like-btn_active");
-    setTimeout(() => {
-      el.style["-webkit-transform"] = "scale(1) translateZ(0)";
-    }, 100);
-  } else {
-    el.style["-webkit-transform"] = "scale(0) translateZ(0)";
-    el.classList.remove("cards__like-btn_active");
-    setTimeout(() => {
-      el.style.transform = "scale(1) translateZ(0)";
-    }, 100);
-  }
-}
-function renderPopupImage(item) {
-  const image = document.querySelector(".popup__image");
-  const imageText = document.querySelector(".popup__image-text");
+  evt.target.classList.toggle('cards__like-btn_active')
 }
 
 /*    Функция открытия Popup             */
 
 function openPopup(popup) {
-  renderInput(popup);
+
   popup.classList.add("popup_opened");
 }
 
@@ -143,43 +139,43 @@ function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-/*   Функция рендера Карточек        */
 
-function renderCards() {
-  const cardsArray = [];
-  for (let i = 0; i < initialCards.length; i++) {
-    cardsArray[i] = createCard()
-    const image = cardsArray[i].querySelector('.cards__image')
-    const title = cardsArray[i].querySelector('.cards__title')
-    image.src = initialCards[i].link
-    title.textContent = initialCards[i].name
-
-    cardsContainer.append(...cardsArray)
-  }
-}
 
 /*     Функция для открывания popup и поведение кнопки 'Сохранить'     */
 
+function handleFormSubmitAddCard(evt) {
+  evt.preventDefault()
+  addedNewCard();
+  closePopup(popupAddCards);
+  evt.target.reset()
+}
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  const form = evt.target;
-  if (form.getAttribute("name") === "popup-form-edit") {
-    profileTitle.textContent = popupInputName.value;
-    profileSubtitle.textContent = popupInputJob.value;
-    closePopup(popupEditProfile);
-  } else {
-    addedNewCard();
-    closePopup(popupAddCards);
-  }
+  profileTitle.textContent = popupInputName.value;
+  profileSubtitle.textContent = popupInputJob.value;
+  closePopup(popupEditProfile);
 }
+
+
+closeBtns.forEach(button => {
+  const popup = button.closest('.popup')
+  button.addEventListener('click', () => closePopup(popup))
+})
 
 /*    Прослушка событий для кнопок close и edit add в профиле     */
 
-popupBtnCloseTypeEdit.addEventListener('click', () => closePopup(popupEditProfile))
-popupBtnCloseTypeAdd.addEventListener('click', () => closePopup(popupAddCards))
-profileAddBtn.addEventListener("click", () => openPopup(popupAddCards));
-profileBtnEdit.addEventListener("click", () => openPopup(popupEditProfile));
-popupForm.forEach(el => el.addEventListener("submit", handleFormSubmit));
+profileAddBtn.addEventListener("click", () => {
+  openPopup(popupAddCards)
+  renderInputAddCards()
+});
+
+profileBtnEdit.addEventListener("click", () => {
+  openPopup(popupEditProfile)
+  renderInputEdit()
+});
+
+popupFormEdit.addEventListener('submit', handleFormSubmit)
+popupFormCards.addEventListener('submit', handleFormSubmitAddCard)
 
 
-renderCards();
+renderCards()
