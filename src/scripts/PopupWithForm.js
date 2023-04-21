27c04c6c89ecb793a399139e634ec12f3c507f40
Gromov Sergey <looneyxx@yandex.ru>
2023-04-21 17:28:26 +0300

    finish

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -3,13 +3,13 @@ export default class PopupWithForm extends Popup {
     constructor(popupSelector, handleSubmitForm) {
         super(popupSelector)
         this._form = this._popup.querySelector('.popup__form')
+        this._inputList = Array.from(this._form.querySelectorAll('.popup__input'))
         this._handleSubmitForm = () => handleSubmitForm(this._getInputValues())
     }
     _getInputValues() {
-        const inputList = Array.from(this._form.querySelectorAll('.popup__input'))
-        const values = inputList.reduce((obj, curr) => {
-            obj[curr.name] = curr.value
-            return obj
+        const values = this._inputList.reduce((values, curr) => {
+            values[curr.name] = curr.value
+            return values
         }, {})
         return values
     }
